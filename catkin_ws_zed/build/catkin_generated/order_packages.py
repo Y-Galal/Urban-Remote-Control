7395c66b2d9072337e35c4be875321bd0f372865
yousef.joe175 <yousef.joe175@gmail.com>
2021-01-20 02:33:20 +0200

    Zed_Ros

---
+++ catkin_ws_zed/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/shelleco/catkin_ws_zed/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/shelleco/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/shelleco/catkin_ws/devel;/opt/ros/melodic" != "" else []
